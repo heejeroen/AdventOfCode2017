5bf860025f6f6f14cc09ce64e6b97ca3b3eabe98
Jeroen Nijs <jnijs@bol.com>
2017-12-08 16:34:50 +0100

    Refactoring of day 6 and 8

--- src/nl/jeroennijs/adventofcode2017/Day06.java
+++ src/nl/jeroennijs/adventofcode2017/Day06.java
@@ -1,6 +1,5 @@
 package nl.jeroennijs.adventofcode2017;
 
-import java.security.MessageDigest;
 import java.util.*;
 
 
@@ -12,76 +11,54 @@ public class Day06 {
     public static void main(String[] args) {
         final int[] initialBanks = { 0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11 };
 
-        System.out.println("Redistributions for part 1: " + calculateRedistributions1(initialBanks)); // 7864
-        System.out.println("Repeat redistributions for part 2: " + calculateRedistributions2(initialBanks)); // 1695
+        System.out.println("Redistributions for part 1: " + calculateRedistributions1(initialBanks, new HashSet<>())); // 7864
+        System.out.println("Repeat redistributions for part 2: " + calculateRedistributions2(initialBanks, new HashSet<>())); // 1695
     }
 
-    private static int calculateRedistributions1(int[] initialBanks) {
-        final MessageDigest digest = Utils.getMd5Digest();
-
-        int[] banks = initialBanks;
+    private static int calculateRedistributions1(int[] banks, Set<Integer> hashes) {
         int nrOfRedistributions = 0;
-        Set<String> hashes = new HashSet<>();
 
-        while (true) {
-            int bankWithHighestNrOfBlocks = getBankWithHighestNrOfBlocks(banks);
-            banks = redistributeBlocks(banks, bankWithHighestNrOfBlocks);
+        do {
+            redistributeBlocks(banks);
             nrOfRedistributions++;
-            if (!hashes.add(Utils.getMd5Hash(digest, printArray(banks)))) {
-                return nrOfRedistributions;
-            }
-        }
+        } while (hashes.add(Arrays.hashCode(banks)));
+
+        return nrOfRedistributions;
     }
 
-    private static int calculateRedistributions2(int[] initialBanks) {
-        final MessageDigest digest = Utils.getMd5Digest();
-
-        int[] banks = initialBanks;
-        Set<String> hashes = new HashSet<>();
-        boolean loopDetected = false;
-        int nrLoop = 0;
-        String hashToDetect = "";
+    private static int calculateRedistributions2(int[] banks, Set<Integer> hashes) {
 
         while (true) {
-            int bankWithHighestNrOfBlocks = getBankWithHighestNrOfBlocks(banks);
-            banks = redistributeBlocks(banks, bankWithHighestNrOfBlocks);
-            String hash = Utils.getMd5Hash(digest, printArray(banks));
-            if (loopDetected) {
-                nrLoop++;
-                if (hash.equals(hashToDetect)) {
-                    return nrLoop;
-                }
-            } else {
-                if (!hashes.add(hash)) {
-                    hashToDetect = hash;
-                    loopDetected = true;
-                }
+            redistributeBlocks(banks);
+            Integer hash = Arrays.hashCode(banks);
+            if (!hashes.add(hash)) {
+                return calculateRedistributionsLoop(banks, hash);
             }
         }
     }
 
-    private static String printArray(int[] banks) {
-        StringBuilder result = new StringBuilder();
-        for (int bank : banks) {
-            result.append(bank);
-            result.append(" ");
-        }
-        return result.toString();
+    private static int calculateRedistributionsLoop(int[] banks, Integer hashToDetect) {
+        int nrOfRedistributions = 0;
+
+        do {
+            redistributeBlocks(banks);
+            nrOfRedistributions++;
+        } while (!hashToDetect.equals(Arrays.hashCode(banks)));
+
+        return nrOfRedistributions;
     }
 
-    private static int[] redistributeBlocks(int[] banks, int bankWithHighestNrOfBlocks) {
+    private static void redistributeBlocks(int[] banks) {
+        final int bankWithHighestNrOfBlocks = getBankWithHighestNrOfBlocks(banks);
         final int nrOfBanks = banks.length;
-        final int[] updatedBanks = new int[nrOfBanks];
         final int blocksToRedistribute = banks[bankWithHighestNrOfBlocks];
         final int extraBlocksPerBank = blocksToRedistribute / nrOfBanks;
         final int remainingBlocks = blocksToRedistribute % nrOfBanks;
 
         for (int i = 0; i < banks.length; i++) {
-            updatedBanks[i] = (i == bankWithHighestNrOfBlocks) ? extraBlocksPerBank - 1 : banks[i] + extraBlocksPerBank;
-            updatedBanks[i] += calculateRemainingBlocks(i, bankWithHighestNrOfBlocks, remainingBlocks, nrOfBanks);
+            banks[i] = (i == bankWithHighestNrOfBlocks) ? extraBlocksPerBank - 1 : banks[i] + extraBlocksPerBank;
+            banks[i] += calculateRemainingBlocks(i, bankWithHighestNrOfBlocks, remainingBlocks, nrOfBanks);
         }
-
-        return updatedBanks;
     }
 
     private static int calculateRemainingBlocks(int i, int bankWithHighestNrOfBlocks, int remainingBlocks, int nrOfBanks) {
--- src/nl/jeroennijs/adventofcode2017/Day08.java
+++ src/nl/jeroennijs/adventofcode2017/Day08.java
@@ -19,34 +19,36 @@ public class Day08 {
         System.out.println("Highest value during execution: " + highestValueDuringExecution); // 6347
     }
 
-    private static int executeInstructions(Map<String, Integer> registers, String fileName) throws IOException {
-        int highestValue = 0;
-        for (String line : Utils.readLines(fileName)) {
-            Matcher matcher = PATTERN.matcher(line);
-            if (matcher.matches()) {
-                if (checkCondition(registers.getOrDefault(matcher.group(4), 0), matcher.group(5), Integer.valueOf(matcher.group(6)))) {
-                    String registerToModify = matcher.group(1);
-                    Integer value = Integer.valueOf(matcher.group(3));
-                    if ("dec".equals(matcher.group(2))) {
-                        value = -value;
-                    }
-                    registers.put(registerToModify, registers.getOrDefault(registerToModify, 0) + value);
-                }
-                highestValue = Math.max(highestValue, determineHighestValue(registers));
-            }
-        }
-        return highestValue;
+    private static int executeInstructions(final Map<String, Integer> registers, String fileName) throws IOException {
+        return Utils.readLines(fileName).stream()
+            .map(PATTERN::matcher)
+            .filter(Matcher::matches)
+            .filter(matcher -> checkCondition(registers.getOrDefault(matcher.group(4), 0), matcher.group(5), Integer.valueOf(matcher.group(6))))
+            .map(matcher -> executeInstruction(registers, matcher.group(1), Integer.valueOf(matcher.group(3)), "dec".equals(matcher.group(2))))
+            .max(Integer::compareTo).orElse(0);
+    }
+
+    private static Integer executeInstruction(Map<String, Integer> registers, String registerToModify, Integer value, boolean isDecrease) {
+        registers.put(registerToModify, registers.getOrDefault(registerToModify, 0) + (isDecrease ? -value : value));
+        return determineHighestValue(registers);
     }
 
     private static boolean checkCondition(Integer value, String conditionOperand, Integer conditionValue) {
         switch (conditionOperand) {
-            case "<": return value < conditionValue;
-            case ">": return value > conditionValue;
-            case "<=": return value <= conditionValue;
-            case ">=": return value >= conditionValue;
-            case "==": return value.equals(conditionValue);
-            case "!=": return !value.equals(conditionValue);
-            default: return false;
+            case "<":
+                return value < conditionValue;
+            case ">":
+                return value > conditionValue;
+            case "<=":
+                return value <= conditionValue;
+            case ">=":
+                return value >= conditionValue;
+            case "==":
+                return value.equals(conditionValue);
+            case "!=":
+                return !value.equals(conditionValue);
+            default:
+                return false;
         }
     }
 
